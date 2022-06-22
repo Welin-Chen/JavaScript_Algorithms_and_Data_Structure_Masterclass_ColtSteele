5fb651905e16d5fef312b7750063111171d7d905
welin <a0929015591@gmail.com>
2022-06-22 21:29:35 +0800

    ch12_Selection_Sort(done)

---
+++ ch12_Selection_Sort/slectionSort.js
@@ -0,0 +1,32 @@
+// function selectionSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let lowest = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[lowest]) {
+//         lowest = j;
+//       }
+//     }
+//     if (i !== lowest) {
+//       console.log(i, lowest);
+//       let temp = arr[i];
+//       arr[i] = arr[lowest];
+//       arr[lowest] = temp;
+//     }
+//   }
+//   return arr;
+// }
+
+function selectionSort(arr) {
+  const swap = (arr, index1, index2) =>
+    ([arr[index1], arr[index2]] = [arr[index2], arr[index1]]);
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) lowest = j;
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+  return arr;
+}
+
+console.log(selectionSort([0, 2, 34, 22, 10, 19, 17, 1]));
